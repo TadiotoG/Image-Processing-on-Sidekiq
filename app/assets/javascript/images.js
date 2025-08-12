0c786234163217e07429e0801b6917113916cbf8
TadiotoG <tadiotogabriel@hotmail.com>
2025-08-11 21:38:21 -0300

    Concertando algumas importações de js

--- app/assets/javascript/images.js
+++ app/assets/javascript/images.js
@@ -1,41 +1,43 @@
-$(document).on('click', '#process-img', function() {
-    let $img_id = $(event.target).data('id');
-    let operation = $(event.target).data('operation');
-    $.ajax({
-        url: "/images/process_img",
-        method: "GET",
-        dataType: "json",
-        data: {
-            process_name: operation,
-            image_id: $(event.target).data('id')
-        },
-        success: function(response) {
-            if (response.status === 202) {
-                let $bg_status = $('[data-bg-id="' + $img_id + '"]')
-                $bg_status
-                    .addClass('bg-warning')
-                    .removeClass('bg-success')
-                    .text('Processando');
+$(document).on('turbo:load', function () {
+        $(document).on('click', '#process-img', function(event) {
+            let $img_id = $(event.target).data('id');
+            let operation = $(event.target).data('operation');
+            $.ajax({
+                url: "/images/process_img",
+                method: "GET",
+                dataType: "json",
+                data: {
+                    process_name: operation,
+                    image_id: $(event.target).data('id')
+                },
+                success: function(response) {
+                    if (response.status === 202) {
+                        let $bg_status = $('[data-bg-id="' + $img_id + '"]')
+                        $bg_status
+                            .addClass('bg-warning')
+                            .removeClass('bg-success')
+                            .text('Processando');
 
-                Swal.fire({
-                icon: "success",    
-                title: "GG",
-                text: `Worker adicionado na fila, sera executado daqui a ${response.time}`
-                });
-            } else {
-                Swal.fire({
-                icon: "error",    
-                title: "ABALO",
-                text: response.message || `Algo deu errado` 
-                });
-            }
-        },
-        error: function() {
-            Swal.fire({
-                icon: "error",    
-                title: "ABALO",
-                text: `Algo deu errado`
+                        Swal.fire({
+                        icon: "success",    
+                        title: "GG",
+                        text: `Worker adicionado na fila, sera executado daqui a ${response.time}`
+                        });
+                    } else {
+                        Swal.fire({
+                        icon: "error",    
+                        title: "ABALO",
+                        text: response.message || `Algo deu errado` 
+                        });
+                    }
+                },
+                error: function() {
+                    Swal.fire({
+                        icon: "error",    
+                        title: "ABALO",
+                        text: `Algo deu errado`
+                    });
+                }
             });
-        }
-    });
+        });
 });
